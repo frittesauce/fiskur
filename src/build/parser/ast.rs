32fe553c35e51fce23f5ba3f0863b3a3387ed02a
frittesauce <9026879@student.zadkine.nl>
2024-12-05 20:19:24 +0100

    just work

---
+++ src/build/parser/ast.rs
@@ -0,0 +1,3 @@
+pub enum idk {
+    string: String
+}
--- src/build/parser/mod.rs
+++ src/build/parser/mod.rs
@@ -1,4 +1,5 @@
 use crate::build::lexer;
+mod ast;
 
 pub fn parser(tokens: Vec<lexer::token::Token>){
     println!("haha im still working on this so here are your totems ig.. {:#?}", tokens);
