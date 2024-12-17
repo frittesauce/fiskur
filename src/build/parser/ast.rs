e132d342971a1074d8bdda5f72fcab139a9e1b70
frittesauce <9026879@student.zadkine.nl>
2024-12-17 08:17:43 +0100

    Woof

--- src/build/parser/ast.rs
+++ src/build/parser/ast.rs
@@ -1,6 +1,14 @@
 use std::{ops::Range, usize};
 
-#[derive(Debug)]
+use toml::Value;
+
+#[derive(Debug, Default)]
+pub struct Location {
+    lines: Range<usize>,
+    columns: Range<usize>,
+}
+
+#[derive(Debug, Default)]
 pub struct Parameter {
     pub location: Location,
     pub mutable: bool,
@@ -13,12 +21,22 @@ pub enum Node {
     #[default]
     Unkown,
     Break,
+    Enum {
+        name: String,
+        fields: Vec<String>
+    },
+    Struct {
+        name: String,
+        fields: Vec<(String, String)>
+    },
     Function {
         identifier: String,
         parameters: Vec<Parameter>,
         return_type: String,
         scope: Vec<NodeInfo>,
     },
+    Scope(Vec<NodeInfo>),
+
     SetVariable {
         identifier: String,
         value: Option<ExprInfo>,
@@ -33,28 +51,44 @@ pub enum Node {
         condition: ExprInfo,
         body: Vec<NodeInfo>,
     },
-    If {
+    IfStatement {
         expression: ExprInfo,
         body: Vec<NodeInfo>,
         elseif: Vec<(ExprInfo, Vec<NodeInfo>)>,
         else_body: Option<Vec<NodeInfo>>,
     },
+    Call(ExprInfo),
+    Return(Option<ExprInfo>),
+    NameSpace {
+        public: bool,
+        static_path: String
+    }
 }
-
-#[derive(Debug)]
-pub struct Location {
-    lines: Range<usize>,
-    columns: Range<usize>,
-}
-
 #[derive(Debug)]
 pub struct NodeInfo {
     location: Location,
     node: Node,
 }
+impl NodeInfo {
+    pub fn void() -> Self {
+        Self {
+            location: Location {
+                columns: 0..0,
+                lines: 0..0
+            },
+            node: Node::Unkown,
+        }
+    }
+}
 
 #[derive(Debug)]
-pub enum Expr {}
+pub enum Expr {
+    GetVariable(String),
+    Field(Box<ExprInfo>, String),
+    Index(String, Box<ExprInfo>),
+    Value(Value),
+    Call(Path, Vec<ExprInfo>)
+}
 
 #[derive(Debug)]
 pub struct ExprInfo {
